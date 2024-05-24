b7ada9c5786ad10316d6ffdb66c5d28c2ea3e8fd
KxD638 <2875636743@qq.com>
2024-05-25 05:22:57 +0800

    B second commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is Student A");
         System.out.println("This is Student B");
+        System.out.println("This is B modification");
     }
 }
